f458555c37be9fc3f61fe6eb6e18e03ea0a33317
marthaberner <marthaberner.mb@gmail.com>
2018-02-26 20:34:33 -0700

    converts input sequence to array of integers

--- ruby/lib/day_1.rb
+++ ruby/lib/day_1.rb
@@ -1,3 +1,11 @@
-class SetUp
+class Captcha
+
+  def initialize(sequence)
+    @sequence = sequence
+  end
+
+  def arrayify_sequence
+    @sequence.to_s.split("").map { |n| n.to_i }
+  end
 
 end
--- ruby/spec/day_1_spec.rb
+++ ruby/spec/day_1_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 require 'day_1'
 
-describe SetUp do
-  it 'works' do
-    expect(true).to eq(true)
+describe Captcha do
+  describe '#arrayify_sequence' do
+    it 'converts input sequence to array of integers' do
+      input = 1122
+      captcha = Captcha.new(input)
+      expect(captcha.arrayify_sequence).to eq [1, 1, 2, 2]
+    end
+
+    it 'returns an empty array if handed nil' do
+      input = nil
+      captcha = Captcha.new(input)
+      expect(captcha.arrayify_sequence).to eq []
+    end
   end
 end
